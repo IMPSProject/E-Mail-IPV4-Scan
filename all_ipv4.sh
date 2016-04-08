fa6d15bb0b7ad0c700182faa4662f994e087af56
Henrique Bissoli Silva <emp.shad@gmail.com>
2016-04-08 15:45:02 -0300

    Update all_ipv4.sh

--- all_ipv4.sh
+++ all_ipv4.sh
@@ -4,9 +4,9 @@ for i in `seq 0 256`; do
 		for k in `seq 0 256`; do
 			for l in `seq 0 256`; do
 				whois $i.$j.$k.$l | grep 'e-mail' | awk '{print $2}' >> global.txt &
-				echo $i.$j.$k.$l
 			done
-			sort global.txt -u -o global.txt
 		done
+		sort global.txt -u -o global.txt
+		"E-mails: "cat global.txt | wc -l
 	done
 done
